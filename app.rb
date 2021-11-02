920a36ea4f605951140e49454b031247ffa28d58
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-11-02 14:26:34 +0000

    Add params functionaliosty to app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+  "This page is secret"
+end
+
+
 get '/cat' do
-  erb(:index)
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
