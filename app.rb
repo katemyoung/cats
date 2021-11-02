333456b16e3e315c533d867384187b278a51ae3c
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-11-02 14:01:08 +0000

    Create views/index.rb

--- app.rb
+++ app.rb
@@ -1,16 +1,6 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  'hello!'
-end
-
-get '/secret' do
-  'This is a very secret page'
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://placekitten.com/500/500'>
-   </div>"
+  erb(:index)
 end
